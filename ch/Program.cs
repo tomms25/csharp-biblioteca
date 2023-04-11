766f88c01ee380255836f81b8f99bde1e7d51f75
Tommaso <tommasobaccano@gmail.com>
2023-04-11 15:41:31 +0200

    Prova commit

---
+++ ch/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections.Generic;
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+var utenti = new List<Utente>();
+var documenti = new List<Documento>();
+var prenotazioni = new List<Prenotazione>();
